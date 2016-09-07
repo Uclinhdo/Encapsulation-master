a8ff5d4f81b5690747729e0c42f041ac52d624f9
ldo <ldo@L11401043624.wctc.msft>
2016-09-07 12:01:53 -0500

    create a new class named HRDepartment.

---
+++ src/lab1/HRDepartment.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author ldo
+ */
+public class HRDepartment {
+    Employee employee;
+     public void meetNewEmployee(String firstname, String lastname, String ssn){
+         Employee em = new Employee();
+         
+     }
+}
